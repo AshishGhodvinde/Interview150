40a2b2971ac9af2748a772c8ede8735f3906e7d1
AshishGhodvinde <ghodvindeashish@gmail.com>
2026-08-04 23:50:12 +0530

    Middle of the LinkedList

---
+++ MiddleOfTheLinkedlist.java
@@ -0,0 +1,26 @@
+import java.util.*;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        
+        // fast and slow pointers will be used, when fast goes 2 steps slow goes 1 step. hence when fast reaches last, slow will be at exact middle. so return slow. simple
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
